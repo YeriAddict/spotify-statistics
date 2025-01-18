40ec8c4373d119162345f8195a528a545969ccef
YeriAddict <denis.leang@gmail.com>
2025-01-18 14:07:38 -0500

    Updated packages

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
-import { nextui } from '@nextui-org/theme'
+import { heroui } from "@heroui/theme";
 
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: [
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
-    './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}'
+    "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
     extend: {
@@ -17,7 +17,7 @@ module.exports = {
   },
   darkMode: "class",
   plugins: [
-    nextui({
+    heroui({
       themes: {
         light: {
           colors: {
@@ -84,7 +84,7 @@ module.exports = {
               DEFAULT: "#FF72A1",
             },
           },
-        },        
+        },
         dark: {
           colors: {
             background: "#000F30",
@@ -154,4 +154,4 @@ module.exports = {
       },
     }),
   ],
-}
+};
